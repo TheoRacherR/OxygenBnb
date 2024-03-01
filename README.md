d3c9df1a8d44162dccbe629696fe8608922b86b3
TheoRacherR <theo.racher-raulin@outlook.com>
2024-03-01 22:56:45 +0100

    Extract docker-compose.yml from back folder

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# OxygenBnb
+# OxygenBnb Application
 Copy of AirBnb
+
+## Build project
+
+### 1. Get the project locally 
+```
+git clone git@github.com:TheoRacherR/OxygenBnb.git
+```
+
+### 2. Create a .env file with those variables (that you have to fill)
+```
+DB_TYPE=
+PG_HOST=
+PG_USER=
+PG_PASSWORD=
+PG_DB=
+PG_PORT=
+PG_PORT_E2E=
+NEST_PORT=
+jwt_secret=
+NODE_ENV_DEV=true or false
+```
+
+### 3. Finally, Build the project
+```
+docker compose up --build
+```
+
+
+## Migrations
+##### Inside "nestapp-oxygen" container 
+```
+npm run typeorm:create-migration
+```
+or
+```
+npm run typeorm:run-migration
+```
+or
+```
+npm run typeorm:revert-migration
+```
--- back/oxygenbnb/src/database/database.module.ts
+++ back/oxygenbnb/src/database/database.module.ts
@@ -13,22 +13,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         username: configService.getOrThrow("PG_USER"),
         password: configService.getOrThrow("PG_PASSWORD"),
         autoLoadEntities: true,
-        synchronize: configService.getOrThrow("NODE_ENV") === "production" ? false : true,
+        synchronize: configService.getOrThrow("NODE_ENV_DEV") === "false" ? false : true,
       }),
       inject: [ConfigService],
     }),
   ],
 })
 export class DatabaseModule {}
-
-// ConfigModule.forRoot(),
-//     TypeOrmModule.forRoot({
-//       type: process.env.DB_TYPE as any,
-//       host: process.env.PG_HOST,
-//       port: parseInt(process.env.PG_PORT),
-//       username: process.env.PG_USER,
-//       password: process.env.PG_PASSWORD,
-//       database: process.env.PG_DB,
-//       entities: [__dirname + "/**/*.entity{.ts,.js}"],
-//       synchronize: process.env.NODE_ENV === "production" ? false : true,
-//     }),
