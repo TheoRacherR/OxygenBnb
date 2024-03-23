63e0dd9335dc0377e2a4e7b5e4317de2d79f0901
TheoRacherR <theo.racher-raulin@outlook.com>
2024-03-23 18:06:32 +0100

    put store informations in localStorage

--- front/oxygenbnb/src/utils/Context/SearchContext.tsx
+++ front/oxygenbnb/src/utils/Context/SearchContext.tsx
@@ -4,14 +4,43 @@ import { createContext, useEffect, useState } from "react";
 export const SearchContext = createContext(undefined)
 
 export const SearchContextProvider = ({children}) => {
-    const [citySelected, setCitySelected] = useState({});
-    const [numberOfPeopleSelected, setNumberOfPeopleSelected] = useState<{adult: number, children: number}>({adult: 1, children: 0});
-    const [numberOfNightSelected, setNumberOfNightSelected] = useState<number>(1);
-    const [nightSelected, setNightSelected] = useState<{start: string, end: string}>({start: dayjs(new Date()).toString(), end: dayjs(new Date()).add(1, 'day').toString()});
-    useEffect(() => {
-      // console.log(nightSelected.start)
-    }, [nightSelected.start])
+    const [citySelected, setCitySelected] = useState(
+      JSON.parse(localStorage.getItem("citySelected")) || {}
+    );
+    const [numberOfPeopleSelected, setNumberOfPeopleSelected] = useState<{adult: number, children: number}>(
+      JSON.parse(localStorage.getItem("numberOfPeopleSelected")) || {adult: 1, children: 0}
+    );
+    const [numberOfNightSelected, setNumberOfNightSelected] = useState<number>(
+      JSON.parse(localStorage.getItem("numberOfNightSelected")) || 1
+    );
+    const [nightSelected, setNightSelected] = useState<{start: string, end: string}>(
+      JSON.parse(localStorage.getItem("nightSelected")) || {start: dayjs(new Date()).toString(), end: dayjs(new Date()).add(1, 'day').toString()}
+    );
     
+    useEffect(() => {
+      if(JSON.parse(localStorage.getItem("citySelected")) !== citySelected) {
+        localStorage.setItem("citySelected", JSON.stringify(citySelected))
+      }
+    }, [citySelected])
+
+    useEffect(() => {
+      if(JSON.parse(localStorage.getItem("numberOfPeopleSelected")) !== numberOfPeopleSelected) {
+        localStorage.setItem("numberOfPeopleSelected", JSON.stringify(numberOfPeopleSelected))
+      }
+    }, [numberOfPeopleSelected])
+
+    useEffect(() => {
+      if(JSON.parse(localStorage.getItem("numberOfNightSelected")) !== numberOfNightSelected) {
+        localStorage.setItem("numberOfNightSelected", JSON.stringify(numberOfNightSelected))
+      }
+    }, [numberOfNightSelected])
+
+    useEffect(() => {
+      if(JSON.parse(localStorage.getItem("nightSelected")) !== nightSelected) {
+        localStorage.setItem("nightSelected", JSON.stringify(nightSelected))
+      }
+    }, [nightSelected])
+
     return (
       <SearchContext.Provider value={{
         citySelected, setCitySelected, 
